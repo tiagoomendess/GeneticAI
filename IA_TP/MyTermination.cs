39fe2bc7cbf77bd2aaadc0de77e64b8dc01629f3
Tiago <tiago@mendes.com.pt>
2017-12-29 13:39:47 +0000

    asdasd

--- IA_TP/MyTermination.cs
+++ IA_TP/MyTermination.cs
@@ -1,19 +1,47 @@
 ﻿using GeneticSharp.Domain;
 using GeneticSharp.Domain.Terminations;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace IA_TP
 {
     class MyTermination : TerminationBase
     {
+        
+        private static bool stop = false;
+
         protected override bool PerformHasReached(IGeneticAlgorithm geneticAlgorithm)
         {
-
-            throw new NotImplementedException();
+            return stop;
         }
+
+        public void Run4Ever()
+        {
+            char c = 'a';
+
+            Thread.Sleep(100);
+
+            while (!stop)
+            {
+                c = Console.ReadKey().KeyChar;
+
+                if (c == 'p')
+                {
+                    stop = true;
+                }
+
+                if (c == 's')
+                {
+                    Helper.ExportCsv(Program.ga.BestChromosome);
+                    Helper.ExportStats(Program.fitList);
+                }
+
+                Thread.Sleep(100);
+            }
+
+        }
+
     }
+
+    
 }
--- IA_TP/Program.cs
+++ IA_TP/Program.cs
@@ -1,53 +1,78 @@
 ﻿using GeneticSharp.Domain;
+using GeneticSharp.Domain.Chromosomes;
 using GeneticSharp.Domain.Crossovers;
+using GeneticSharp.Domain.Fitnesses;
 using GeneticSharp.Domain.Mutations;
 using GeneticSharp.Domain.Populations;
 using GeneticSharp.Domain.Selections;
 using GeneticSharp.Domain.Terminations;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace IA_TP
 {
     class Program
     {
+        static ISelection selection = new EliteSelection();
+        static ICrossover crossover = new UniformCrossover();
+        static IMutation mutation = new ReverseSequenceMutation();
+        static Fitness fitness = new Fitness();
+        static Cromosoma chromosome = new Cromosoma();
+        static Population population = new Population(500, 500, chromosome);
+        public static GeneticAlgorithm ga = new GeneticAlgorithm(population, fitness, selection, crossover, mutation);
+        public static string fitList;
 
-        public static int DIAS = 28;
         static void Main(string[] args)
         {
             Console.WriteLine("A iniciar programa...");
+            fitList = "";
+            string bestGenes = "";
+            Thread t, j;
 
-            var selection = new EliteSelection();
-            var crossover = new UniformCrossover();
-            var mutation = new ReverseSequenceMutation();
-            var fitness = new Fitness();
-            var chromosome = new Cromosoma();
-            var population = new Population(500, 500, chromosome);
-
-            var ga = new GeneticAlgorithm(population, fitness, selection, crossover, mutation);
-            ga.Termination = new OrTermination(new GenerationNumberTermination(100000), new FitnessThresholdTermination(99));
+            ga.Termination = new OrTermination(new GenerationNumberTermination(200000), new FitnessThresholdTermination(98), new MyTermination());
 
             Console.WriteLine("Algoritmo a correr...");
 
+            //Event handler para ser executado a cada geração
             ga.GenerationRan += delegate
             {
-                Console.Clear();
-                var bestChromosome = ga.Population.BestChromosome;
-                Console.WriteLine("Gerações: {0}", ga.Population.GenerationsNumber);
-                Console.WriteLine("Fitness: {0,10}", bestChromosome.Fitness);
-                Console.WriteLine("Tempo: {0}h {1}m {2}s", ga.TimeEvolving.Hours, ga.TimeEvolving.Minutes, ga.TimeEvolving.Seconds);
-                Cromosoma.BuildCalendar(bestChromosome.GetGenes());
+
+                    Console.Clear();
+                    var bestChromosome = ga.Population.BestChromosome;
+                    Console.WriteLine("Melhor Cromosoma (Fitness {0}):", bestChromosome.Fitness);
+                    Console.WriteLine(Cromosoma.BuildCalendar(ga.BestChromosome.GetGenes()));
+                    Console.WriteLine("Geração: {0}", ga.Population.GenerationsNumber);
+                    Console.WriteLine("Tempo: {0}h {1}m {2}s", ga.TimeEvolving.Hours, ga.TimeEvolving.Minutes, ga.TimeEvolving.Seconds);
+
             };
 
+            //Ouvir o teclado para poder parar a meio se pressionar a tecla p
+            t = new Thread(new MyTermination().Run4Ever);
+            t.Start();
+
+            //Gravar stats em real time
+            j = new Thread(new Helper().AddFitnessToList);
+            j.Start();
+
+            //Começar
             ga.Start();
 
-            Console.WriteLine("A melhor solução encontrada teve {0} de fitness.", ga.BestChromosome.Fitness);
+            Console.Clear();
+            Console.WriteLine("Melhor Cromosoma (Fitness {0}):", ga.BestChromosome.Fitness);
             Console.WriteLine(Cromosoma.BuildCalendar(ga.BestChromosome.GetGenes()));
+            Console.WriteLine("Total de Gerações: {0}", ga.Population.GenerationsNumber);
+            Console.WriteLine("Tempo Total: {0}h {1}m {2}s", ga.TimeEvolving.Hours, ga.TimeEvolving.Minutes, ga.TimeEvolving.Seconds);
+            Console.WriteLine("O Algoritmo Parou!");
+
             Helper.ExportCsv(ga.BestChromosome);
+            Helper.ExportStats(fitList);
             Console.ReadLine();
         }
+
     }
 }
